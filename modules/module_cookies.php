72dc45a669cbf9cd526084f57b5bdb54882bd4b1
Caleb Mazalevskis <maikuolan@gmail.com>
2017-01-02 05:42:56 +0800

    Modules update.
    Added cookie scanner; Updated security extras.

---
+++ modules/module_cookies.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * This file is a part of the CIDRAM package, and can be downloaded for free
+ * from {@link https://github.com/Maikuolan/CIDRAM/ GitHub}.
+ *
+ * CIDRAM COPYRIGHT 2016 and beyond by Caleb Mazalevskis (Maikuolan).
+ *
+ * License: GNU/GPLv2
+ * @see LICENSE.txt
+ *
+ * This file: Optional cookie scanner module (last modified: 2017.01.02).
+ *
+ * Many thanks to Michael Hopkins, the creator of ZB Block (GNU/GPLv2) and its
+ * cookie scanner module, which the cookie scanner module for CIDRAM is based
+ * upon and inspired by.
+ */
+
+/** Prevents execution from outside of CIDRAM. */
+if (!defined('CIDRAM')) {
+    die('[CIDRAM] This should not be accessed directly.');
+}
+
+/** Required for handling all signature triggers in this file. */
+$Trigger = function ($Condition, $ReasonShort, $ReasonLong = '', $DefineOptions = array()) use (&$CIDRAM) {
+    if (!$Condition) {
+        return false;
+    }
+    if (!$ReasonLong) {
+        $ReasonLong = $ReasonShort;
+    }
+    if (is_array($DefineOptions) && !empty($DefineOptions)) {
+        while (($Cat = each($DefineOptions)) !== false) {
+            while (($Option = each($Cat[1])) !== false) {
+                $CIDRAM['Config'][$Cat[0]][$Option[0]] = $Option[1];
+            }
+        }
+    }
+    $CIDRAM['BlockInfo']['ReasonMessage'] = $ReasonLong;
+    if (!empty($CIDRAM['BlockInfo']['WhyReason'])) {
+        $CIDRAM['BlockInfo']['WhyReason'] .= ', ';
+    }
+    $CIDRAM['BlockInfo']['WhyReason'] .= $ReasonShort;
+    if (!empty($CIDRAM['BlockInfo']['Signatures'])) {
+        $CIDRAM['BlockInfo']['Signatures'] .= ', ';
+    }
+    $Debug = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT | DEBUG_BACKTRACE_IGNORE_ARGS, 1)[0];
+    $CIDRAM['BlockInfo']['Signatures'] .= basename($Debug['file']) . ':L' . $Debug['line'];
+    $CIDRAM['BlockInfo']['SignatureCount']++;
+    return true;
+};
+
+$Cookies = count($_COOKIE);
+
+if (!$Trigger($Cookies > 30, 'Cookie flood', 'Cookie flood detected!') && $Cookies) {
+    array_walk($_COOKIE, function($Value, $Key) use (&$CIDRAM, &$Trigger) {
+        $KeyLC = strtolower($Key);
+        $ValueLC = strtolower($Value);
+        $ThisPair = $Key . '->' . $Value;
+        $ThisPairN = preg_replace('/\s/', '', strtolower($ThisPair));
+
+        $Trigger(preg_match('/(?:\+A(?:CI|D[sw4]|[FH][s0]|GA)-|U\+003[EC])/', $ThisPair), 'UTF-7 entities detected in cookie'); // 2017.01.02
+
+        $Trigger(preg_match('/\((?:["\']{2})?\)/', $ThisPairN), 'Command injection'); // 2017.01.02
+        $Trigger(preg_match(
+            '/(?:_once|able|as(c|hes|sert)|c(hr|ode|ontents)|e(cho|regi|scape|va' .
+            'l)|ex(ec|ists)?|f(ile|late|unction)|hex2bin|get(c|csv|ss?)?|i(f|ncl' .
+            'ude)|len(gth)?|nt|open|p(ress|lace|lode|uts)|print(f|_r)?|re(ad|pla' .
+            'ce|quire|store)|rot13|s(tart|ystem)|w(hile|rite))["\':(\[{<$]/i',
+        $ThisPairN), 'Command injection'); // 2017.01.02
+        $Trigger(
+            preg_match('/\$(?:globals|_cookie|_env|_files|_get|_post|_request|_server|_session)/i', $ThisPairN),
+            'Command injection'
+        ); // 2017.01.02
+        $Trigger(preg_match('/add(?:handler|type|inputfilter)/', $ThisPairN), 'Command injection'); // 2017.01.02
+        $Trigger(preg_match('/http_(?:cmd|sum)/', $ThisPairN), 'Command injection'); // 2017.01.02
+        $Trigger(preg_match('/pa(?:rse_ini_file|ssthru)/', $ThisPairN), 'Command injection'); // 2017.01.02
+        $Trigger(preg_match('/rewrite(?:cond|rule)/', $ThisPairN), 'Command injection'); // 2017.01.02
+        $Trigger(preg_match('/set(?:handler|inputfilter)/', $ThisPairN), 'Command injection'); // 2017.01.02
+        $Trigger(preg_match('/u(?:nserialize|ploadedfile)/', $ThisPairN), 'Command injection'); // 2017.01.02
+        $Trigger(strpos($ThisPairN, '$http_raw_post_data') !== false, 'Command injection'); // 2017.01.02
+        $Trigger(strpos($ThisPairN, 'dotnet_load') !== false, 'Command injection'); // 2017.01.02
+        $Trigger(strpos($ThisPairN, 'execcgi') !== false, 'Command injection'); // 2017.01.02
+        $Trigger(strpos($ThisPairN, 'forcetype') !== false, 'Command injection'); // 2017.01.02
+        $Trigger(strpos($ThisPairN, 'move_uploaded_file') !== false, 'Command injection'); // 2017.01.02
+        $Trigger(strpos($ThisPairN, 'symlink') !== false, 'Command injection'); // 2017.01.02
+        $Trigger(strpos($ThisPairN, 'tmp_name') !== false, 'Command injection'); // 2017.01.02
+        $Trigger(strpos($ThisPairN, '_contents') !== false, 'Command injection'); // 2017.01.02
+
+        $Trigger(preg_match('/ap(?:ache_[a-z0-9_]{4,16}|c_[a-z0-9_]{3,16})\(/', $ThisPairN), 'Function call detected in cookie'); // 2017.01.02
+        $Trigger(preg_match('/curl_[a-z0-9_]{4,10}\(/', $ThisPairN), 'Function call detected in cookie'); // 2017.01.02
+        $Trigger(preg_match('/ftp_[a-z0-9_]{3,7}\(/', $ThisPairN), 'Function call detected in cookie'); // 2017.01.02
+        $Trigger(preg_match('/mysqli?(?:_|\:\:)[a-z0-9_]{4,9}\(/', $ThisPairN), 'Function call detected in cookie'); // 2017.01.02
+        $Trigger(preg_match('/phpads_[a-z0-9_]{4,12}\(/', $ThisPairN), 'Function call detected in cookie'); // 2017.01.02
+        $Trigger(preg_match('/posix_[a-z0-9_]{4,19}\(/', $ThisPairN), 'Function call detected in cookie'); // 2017.01.02
+        $Trigger(preg_match('/proc_[a-z0-9_]{4,10}\(/', $ThisPairN), 'Function call detected in cookie'); // 2017.01.02
+
+        $Trigger(preg_match('/\'(?:uploadedfile|move_uploaded_file|tmp_name)\'/', $ThisPairN), 'Probe attempt detected'); // 2017.01.02
+
+        $Trigger(strpos($ThisPairN, '@$' . '_[' . ']=' . '@!' . '+_') !== false, 'Shell upload attempt'); // 2017.01.02
+        $Trigger(strpos($ThisPairN, 'linkirc') !== false, 'Shell upload attempt'); // 2017.01.02
+
+        $Trigger($Key === 'SESSUNIVUCADIACOOKIE', 'Hotlinking detected', 'Hotlinking not allowed!'); // 2017.01.02
+
+        $Trigger($Key === 'arp_scroll_position' && strpos($ThisPairN, '400') !== false, 'Bad cookie'); // 2017.01.02
+        $Trigger($Key === 'BALANCEID' && strpos($ThisPairN, 'balancer.') !== false, 'Bad cookie'); // 2017.01.02
+        $Trigger($Key === 'BX', 'Bad cookie'); // 2017.01.02
+        $Trigger($Key === 'ja_edenite_tpl', 'Bad cookie'); // 2017.01.02
+        $Trigger($Key === 'phpbb3_1fh61_', 'Bad cookie'); // 2017.01.02
+
+        $Trigger($Key === '()' || $Value === '()', 'Bash/Shellshock attempt'); // 2017.01.02
+
+        $Trigger((
+            ($Key == 'CUSTOMER' || $Key == 'CUSTOMER_INFO' || $Key == 'NEWMESSAGE') &&
+            strpos($ThisPairN, 'deleted') !== false
+        ), 'Hack attempt detected'); // 2017.01.02
+
+        $Trigger($KeyLC === 'rm -rf' || $ValueLC === 'rm -rf', 'Hack attempt detected'); // 2017.01.02
+
+        $Trigger((
+            ($Value == -1 || $Value == '-1') &&
+            ($Key == 'ASP_NET_SessionId' || $Key == 'CID' || $Key == 'SID' || $Key == 'NID')
+        ), 'ASP.NET hack detected'); // 2017.01.02
+
+    });
+}
--- modules/module_extras.php
+++ modules/module_extras.php
@@ -8,7 +8,11 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Optional security extras module (last modified: 2016.12.31).
+ * This file: Optional security extras module (last modified: 2017.01.02).
+ *
+ * Many thanks to Michael Hopkins, the creator of ZB Block (GNU/GPLv2), and to
+ * the community behind it (Spambot Security) for inspiring/developing many of
+ * the signatures contained within this module.
  */
 
 /** Prevents execution from outside of CIDRAM. */
@@ -61,34 +65,104 @@ if (!empty($_SERVER['QUERY_STRING'])) {
         '/(?:_once|able|as(c|hes|sert)|c(hr|ode|ontents)|e(cho|regi|scape|va' .
         'l)|ex(ec|ists)?|f(ile|late|unction)|get(c|csv|ss?)?|i(f|nclude)|len' .
         '(gth)?|nt|open|p(ress|lace|lode|uts)|print(f|_r)?|re(ad|place|quire' .
-        '|store)|rot13|s(tart|ystem)|w(hile|rite))["\':(\[{<$]/i',
+        '|store)|rot13|s(tart|ystem)|w(hile|rite))["\':(\[{<$]/',
     $QueryNoSpace), 'Command injection'); // 2016.12.31
     $Trigger(
-        preg_match('/\$(?:globals|_cookie|_env|_files|_get|_post|_request|_server|_session)/i', $QueryNoSpace),
+        preg_match('/\$(?:globals|_cookie|_env|_files|_get|_post|_request|_server|_session)/', $QueryNoSpace),
         'Command injection'
     ); // 2016.12.31
+    $Trigger(preg_match('/http_(?:cmd|sum)/', $QueryNoSpace), 'Command injection'); // 2017.01.02
+    $Trigger(preg_match('/pa(?:rse_ini_file|ssthru)/', $QueryNoSpace), 'Command injection'); // 2017.01.02
+    $Trigger(preg_match('/rewrite(?:cond|rule)/', $QueryNoSpace), 'Command injection'); // 2017.01.02
+    $Trigger(preg_match('/u(?:nserialize|ploadedfile)/', $QueryNoSpace), 'Command injection'); // 2017.01.02
     $Trigger(strpos($QueryNoSpace, 'dotnet_load') !== false, 'Command injection'); // 2016.12.31
     $Trigger(strpos($QueryNoSpace, 'execcgi') !== false, 'Command injection'); // 2016.12.31
-    $Trigger(strpos($QueryNoSpace, 'http_cmd') !== false, 'Command injection'); // 2016.12.31
-    $Trigger(strpos($QueryNoSpace, 'http_cmd') !== false, 'Command injection'); // 2016.12.31
-    $Trigger(strpos($QueryNoSpace, 'http_sum') !== false, 'Command injection'); // 2016.12.31
     $Trigger(strpos($QueryNoSpace, 'move_uploaded_file') !== false, 'Command injection'); // 2016.12.31
-    $Trigger(strpos($QueryNoSpace, 'parse_ini_file') !== false, 'Command injection'); // 2016.12.31
-    $Trigger(strpos($QueryNoSpace, 'passthru') !== false, 'Command injection'); // 2016.12.31
-    $Trigger(strpos($QueryNoSpace, 'rewritecond') !== false, 'Command injection'); // 2016.12.31
-    $Trigger(strpos($QueryNoSpace, 'rewriterule') !== false, 'Command injection'); // 2016.12.31
     $Trigger(strpos($QueryNoSpace, 'symlink') !== false, 'Command injection'); // 2016.12.31
     $Trigger(strpos($QueryNoSpace, 'tmp_name') !== false, 'Command injection'); // 2016.12.31
-    $Trigger(strpos($QueryNoSpace, 'unserialize') !== false, 'Command injection'); // 2016.12.31
-    $Trigger(strpos($QueryNoSpace, 'uploadedfile') !== false, 'Command injection'); // 2016.12.31
     $Trigger(strpos($QueryNoSpace, '_contents') !== false, 'Command injection'); // 2016.12.31
 
     $Trigger(preg_match('/%(?:0[0-8bcef]|1)/i', $_SERVER['QUERY_STRING']), 'Non-printable characters in query'); // 2016.12.31
-    
+
     $Trigger(preg_match('/(?:amp(;|%3b)){2,}/', $QueryNoSpace), 'Nesting attack'); // 2016.12.31
-    
+    $Trigger((
+        strpos($CIDRAM['BlockInfo']['rURI'], '/ucp.php?mode=login') === false &&
+        preg_match('/%(?:(25){2,}|(25)+27)/', $_SERVER['QUERY_STRING'])
+    ), 'Nesting attack'); // 2017.01.01
+
+    $Trigger(
+        preg_match('/(?:<(\?|body|object|script)|(body|object|script)>)/', $QueryNoSpace),
+        'Script injection'
+    ); // 2017.01.01
+
+    $Trigger(strpos($QueryNoSpace, '1http:'), 'RFI'); // 2017.01.01
+    $Trigger(preg_match('/\|(?:include|require)/', $QueryNoSpace), 'RFI'); // 2017.01.01
+
+    $Trigger(preg_match('/_(?:cookie|env|files|(ge|pos|reques)t|s(erver|ession))\[/', $QueryNoSpace), 'Global variable hack'); // 2017.01.01
+    $Trigger(strpos($QueryNoSpace, 'globals['), 'Global variable hack'); // 2017.01.01
+
     $Trigger(substr($_SERVER['QUERY_STRING'], -3) === '%00', 'Null truncation attempt'); // 2016.12.31
     $Trigger(substr($_SERVER['QUERY_STRING'], -4) === '%000', 'Null truncation attempt'); // 2016.12.31
     $Trigger(substr($_SERVER['QUERY_STRING'], -5) === '%0000', 'Null truncation attempt'); // 2016.12.31
+
+    $Trigger(strpos($QueryNoSpace, '@$' . '_[' . ']=' . '@!' . '+_') !== false, 'Shell upload attempt'); // 2017.01.02
+
+    $Trigger(strpos($Query, 'rm -rf') !== false, 'Hack attempt detected'); // 2017.01.02
+
+    $Trigger(count($_REQUEST) >= 500, 'Hack attempt', 'Too many request variables sent!'); // 2017.01.01
+
+}
+
+/* Some checks against the UA (user agent). */
+if ($CIDRAM['BlockInfo']['UA'] && !$Trigger(strlen($CIDRAM['BlockInfo']['UA']) > 4096, 'Bad UA', 'User agent string is too long!')) {
+    $UA = strtolower(urldecode($CIDRAM['BlockInfo']['UA']));
+    $UANoSpace = preg_replace('/\s/', '', $UA);
+
+    $Trigger(preg_match('/\((?:["\']{2})?\)/', $UANoSpace), 'Command injection'); // 2017.01.02
+    $Trigger(preg_match(
+        '/(?:_once|able|as(c|hes|sert)|c(hr|ode|ontents)|e(cho|regi|scape|va' .
+        'l)|ex(ec|ists)?|f(ile|late|unction)|get(c|csv|ss?)?|i(f|nclude)|len' .
+        '(gth)?|open|p(ress|lace|lode|uts)|print(f|_r)?|re(ad|place|quire|st' .
+        'ore)|rot13|s(tart|ystem)|w(hile|rite))["\':(\[{<$]/',
+    $UANoSpace), 'Command injection'); // 2017.01.02
+    $Trigger(
+        preg_match('/\$(?:globals|_cookie|_env|_files|_get|_post|_request|_server|_session)/', $UANoSpace),
+        'Command injection'
+    ); // 2017.01.02
+    $Trigger(preg_match('/http_(?:cmd|sum)/', $UANoSpace), 'Command injection'); // 2017.01.02
+    $Trigger(preg_match('/pa(?:rse_ini_file|ssthru)/', $UANoSpace), 'Command injection'); // 2017.01.02
+    $Trigger(preg_match('/rewrite(?:cond|rule)/', $UANoSpace), 'Command injection'); // 2017.01.02
+    $Trigger(preg_match('/u(?:nserialize|ploadedfile)/', $UANoSpace), 'Command injection'); // 2017.01.02
+    $Trigger(strpos($UANoSpace, 'dotnet_load') !== false, 'Command injection'); // 2017.01.02
+    $Trigger(strpos($UANoSpace, 'execcgi') !== false, 'Command injection'); // 2017.01.02
+    $Trigger(strpos($UANoSpace, 'move_uploaded_file') !== false, 'Command injection'); // 2017.01.02
+    $Trigger(strpos($UANoSpace, 'symlink') !== false, 'Command injection'); // 2017.01.02
+    $Trigger(strpos($UANoSpace, 'tmp_name') !== false, 'Command injection'); // 2017.01.02
+    $Trigger(strpos($UANoSpace, '_contents') !== false, 'Command injection'); // 2017.01.02
+
+    $Trigger(preg_match('/%(?:0[0-8bcef]|1)/i', $CIDRAM['BlockInfo']['UA']), 'Non-printable characters in UA'); // 2017.01.02
+
+    $Trigger(
+        preg_match('/(?:<(\?|body|iframe|object|script)|(body|object|script)>)/', $UANoSpace),
+        'Script injection'
+    ); // 2017.01.02
+
+    $Trigger(preg_match('/_(?:cookie|env|files|(ge|pos|reques)t|s(erver|ession))\[/', $UANoSpace), 'Global variable hack'); // 2017.01.02
+    $Trigger(strpos($UANoSpace, 'globals['), 'Global variable hack'); // 2017.01.02
+
+    $Trigger(strpos($UANoSpace, '$_' . '[$' . '__') !== false, 'Shell upload attempt'); // 2017.01.02
+    $Trigger(strpos($UANoSpace, '@$' . '_[' . ']=' . '@!' . '+_') !== false, 'Shell upload attempt'); // 2017.01.02
+
+    $Trigger(strpos($UANoSpace, '}__') !== false, 'Joomla hack UA'); // 2017.01.02
+
+    $Trigger(strpos($UA, 'rm -rf') !== false, 'Hack UA'); // 2017.01.02
+    $Trigger(strpos($UANoSpace, 'r00t') !== false, 'Hack UA'); // 2017.01.02
+    $Trigger(strpos($UANoSpace, 'shell_exec') !== false, 'Hack UA'); // 2017.01.02
+
+    $Trigger(preg_match('/(?:x(rumer|pymep)|хрумер)/', $UANoSpace), 'Spam UA'); // 2017.01.02
+    $Trigger(preg_match('/[<\[](?:a|link|url)[ =>\]]/', $UA), 'Spam UA'); // 2017.01.02
+    $Trigger(strpos($UANoSpace, 'start.exe') !== false, 'Spam UA'); // 2017.01.02
+
+    $Trigger(strpos($UA, '   ') !== false, 'Bad UA'); // 2017.01.02
 
 }
