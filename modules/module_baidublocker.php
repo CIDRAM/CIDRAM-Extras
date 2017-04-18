e83b1b95f0cefdf09d968debcbedcf83102119c5
Caleb Mazalevskis <maikuolan@gmail.com>
2017-04-18 11:58:44 +0800

    Modules update.
    Baidu blocker module.

--- modules/module_baidublocker.php
+++ modules/module_baidublocker.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Baidu blocker module (last modified: 2017.03.12).
+ * This file: Baidu blocker module (last modified: 2017.04.18).
  */
 
 /** Prevents execution from outside of CIDRAM. */
@@ -41,9 +41,9 @@ IPv4 Signatures (ASNs 38365, 38627, 55967):
 
 45.113.192.0/22 Deny 百度被禁止从这里
 63.243.252.0/24 Deny 百度被禁止从这里
-103.6.76.0/24 Deny 百度被禁止从这里
 103.235.44.0/22 Deny 百度被禁止从这里
 104.193.88.0/23 Deny 百度被禁止从这里
+106.12.0.0/18 Deny 百度被禁止从这里
 119.63.192.0/21 Deny 百度被禁止从这里
 119.75.208.0/20 Deny 百度被禁止从这里
 131.161.8.0/22 Deny 百度被禁止从这里
@@ -56,11 +56,7 @@ IPv4 Signatures (ASNs 38365, 38627, 55967):
 182.61.0.0/17 Deny 百度被禁止从这里
 182.61.132.0/24 Deny 百度被禁止从这里
 182.61.248.0/23 Deny 百度被禁止从这里
-202.46.48.0/21 Deny 百度被禁止从这里
-202.46.56.0/23 Deny 百度被禁止从这里
-202.46.58.0/24 Deny 百度被禁止从这里
-202.46.60.0/23 Deny 百度被禁止从这里
-202.46.62.0/24 Deny 百度被禁止从这里
+202.46.48.0/20 Deny 百度被禁止从这里
 Tag: Baidu IPv4
 ---
 Options:
