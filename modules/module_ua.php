d3f2b0e44f3d3ad68856b55cda0b4a3eae7657c1
Caleb Mazalevskis <maikuolan@gmail.com>
2017-08-12 22:15:32 +0800

    Empty UA module.

---
+++ modules/module_ua.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is a part of the CIDRAM package.
+ * Homepage: https://cidram.github.io/
+ *
+ * CIDRAM COPYRIGHT 2016 and beyond by Caleb Mazalevskis (Maikuolan).
+ *
+ * License: GNU/GPLv2
+ * @see LICENSE.txt
+ *
+ * This file: Empty UA blocker module (last modified: 2017.08.12).
+ */
+
+/** Prevents execution from outside of CIDRAM. */
+if (!defined('CIDRAM')) {
+    die('[CIDRAM] This should not be accessed directly.');
+}
+
+/** Inherit trigger closure (see functions.php). */
+$Trigger = $CIDRAM['Trigger'];
+
+/** Block blank/empty user agents. */
+$Trigger(preg_replace('~[^a-z0-9]~i', '', $CIDRAM['BlockInfo']['UA']) == '', 'Empty UA');
