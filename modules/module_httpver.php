2e0ec4c611a24ecc4af3f916bc52ab85ae197ec9
Caleb Mazalevskis <maikuolan@gmail.com>
2021-01-16 15:10:09 +0800

    Fix HTTP version checker bug.

--- modules/module_httpver.php
+++ modules/module_httpver.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: HTTP version checker module (last modified: 2021.01.15).
+ * This file: HTTP version checker module (last modified: 2021.01.16).
  *
  * False positive risk (an approximate, rough estimate only): « [x]Low [ ]Medium [ ]High »
  */
@@ -34,6 +34,7 @@ $CIDRAM['ModuleResCache'][$Module] = function () use (&$CIDRAM) {
     }
     $Major = (int)$Version[0];
     $Minor = isset($Version[1]) ? (int)$Version[1] : 0;
+    $Rebuilt = $Protocol . '/' . $Major . '.' . $Minor;
 
     /** Inherit trigger closure (see functions.php). */
     $Trigger = $CIDRAM['Trigger'];
@@ -41,43 +42,43 @@ $CIDRAM['ModuleResCache'][$Module] = function () use (&$CIDRAM) {
     if ($Protocol === 'HTTP') {
         /** See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Evolution_of_HTTP */
         $Trigger((
-            ($CIDRAM['Config']['httpver']['http/0.9'] && $Major === 0 && $Minor === 9) ||
-            ($CIDRAM['Config']['httpver']['http/0.x'] && $Major === 0 && $Minor !== 9) ||
-            ($CIDRAM['Config']['httpver']['http/1.0'] && $Major === 1 && $Minor === 0) ||
-            ($CIDRAM['Config']['httpver']['http/1.1'] && $Major === 1 && $Minor === 1) ||
-            ($CIDRAM['Config']['httpver']['http/1.2'] && $Major === 1 && $Minor === 2) ||
-            ($CIDRAM['Config']['httpver']['http/1.3'] && $Major === 1 && $Minor === 3) ||
-            ($CIDRAM['Config']['httpver']['http/1.x'] && $Major === 1 && $Minor !== 0 && $Minor !== 1 && $Minor !== 2 && $Minor !== 3) ||
-            ($CIDRAM['Config']['httpver']['http/2.0'] && $Major === 2 && $Minor === 0) ||
-            ($CIDRAM['Config']['httpver']['http/2.x'] && $Major === 2 && $Minor !== 0) ||
-            ($CIDRAM['Config']['httpver']['http/3.0'] && $Major === 3 && $Minor === 0) ||
-            ($CIDRAM['Config']['httpver']['http/3.x'] && $Major === 3 && $Minor !== 0) ||
-            ($CIDRAM['Config']['httpver']['http/other'] && $Major !== 0 && $Major !== 1 && $Major !== 2 && $Major !== 3)
-        ), 'Protocol denied', $CIDRAM['Config']['httpver']['reason_message']);
+            ($CIDRAM['Config']['httpver']['http_zero_nine'] && $Major === 0 && $Minor === 9) ||
+            ($CIDRAM['Config']['httpver']['http_zero_x'] && $Major === 0 && $Minor !== 9) ||
+            ($CIDRAM['Config']['httpver']['http_one_zero'] && $Major === 1 && $Minor === 0) ||
+            ($CIDRAM['Config']['httpver']['http_one_one'] && $Major === 1 && $Minor === 1) ||
+            ($CIDRAM['Config']['httpver']['http_one_two'] && $Major === 1 && $Minor === 2) ||
+            ($CIDRAM['Config']['httpver']['http_one_three'] && $Major === 1 && $Minor === 3) ||
+            ($CIDRAM['Config']['httpver']['http_one_x'] && $Major === 1 && $Minor !== 0 && $Minor !== 1 && $Minor !== 2 && $Minor !== 3) ||
+            ($CIDRAM['Config']['httpver']['http_two_zero'] && $Major === 2 && $Minor === 0) ||
+            ($CIDRAM['Config']['httpver']['http_two_x'] && $Major === 2 && $Minor !== 0) ||
+            ($CIDRAM['Config']['httpver']['http_three_zero'] && $Major === 3 && $Minor === 0) ||
+            ($CIDRAM['Config']['httpver']['http_three_x'] && $Major === 3 && $Minor !== 0) ||
+            ($CIDRAM['Config']['httpver']['http_other'] && $Major !== 0 && $Major !== 1 && $Major !== 2 && $Major !== 3)
+        ), 'Protocol denied (' . $Rebuilt . ')', $CIDRAM['Config']['httpver']['reason_message']);
     } elseif ($Protocol === 'SHTTP') {
         $Trigger((
-            ($CIDRAM['Config']['httpver']['shttp/1.3'] && $Major === 1 && $Minor === 3) ||
-            ($CIDRAM['Config']['httpver']['shttp/other'] && !($Major === 1 && $Minor === 3))
-        ), 'Protocol denied', $CIDRAM['Config']['httpver']['reason_message']);
+            ($CIDRAM['Config']['httpver']['shttp_one_three'] && $Major === 1 && $Minor === 3) ||
+            ($CIDRAM['Config']['httpver']['shttp_other'] && !($Major === 1 && $Minor === 3))
+        ), 'Protocol denied (' . $Rebuilt . ')', $CIDRAM['Config']['httpver']['reason_message']);
     } elseif ($Protocol === 'SPDY') {
         $Trigger((
-            ($CIDRAM['Config']['httpver']['spdy/3.0'] && $Major === 3 && $Minor === 0) ||
-            ($CIDRAM['Config']['httpver']['spdy/3.1'] && $Major === 3 && $Minor === 1) ||
-            ($CIDRAM['Config']['httpver']['spdy/other'] && ($Major !== 3 || ($Minor !== 0 && $Minor !== 1)))
-        ), 'Protocol denied', $CIDRAM['Config']['httpver']['reason_message']);
+            ($CIDRAM['Config']['httpver']['spdy_three_zero'] && $Major === 3 && $Minor === 0) ||
+            ($CIDRAM['Config']['httpver']['spdy_three_one'] && $Major === 3 && $Minor === 1) ||
+            ($CIDRAM['Config']['httpver']['spdy_other'] && ($Major !== 3 || ($Minor !== 0 && $Minor !== 1)))
+        ), 'Protocol denied (' . $Rebuilt . ')', $CIDRAM['Config']['httpver']['reason_message']);
     } elseif ($Protocol === 'IRC') {
         /** See: https://tools.ietf.org/html/rfc7230 */
         $Trigger((
-            ($CIDRAM['Config']['httpver']['irc/6.9'] && $Major === 6 && $Minor === 9) ||
-            ($CIDRAM['Config']['httpver']['irc/other'] && !($Major === 6 && $Minor === 9))
-        ), 'Protocol denied', $CIDRAM['Config']['httpver']['reason_message']);
+            ($CIDRAM['Config']['httpver']['irc_six_nine'] && $Major === 6 && $Minor === 9) ||
+            ($CIDRAM['Config']['httpver']['irc_other'] && !($Major === 6 && $Minor === 9))
+        ), 'Protocol denied (' . $Rebuilt . ')', $CIDRAM['Config']['httpver']['reason_message']);
     } elseif ($Protocol === 'RTA') {
         $Trigger((
-            ($CIDRAM['Config']['httpver']['rta/x11'] && $_SERVER['SERVER_PROTOCOL'] === 'RTA/x11') ||
-            ($CIDRAM['Config']['httpver']['rta/other'] && $_SERVER['SERVER_PROTOCOL'] !== 'RTA/x11')
-        ), 'Protocol denied', $CIDRAM['Config']['httpver']['reason_message']);
+            ($CIDRAM['Config']['httpver']['rta_xeleven'] && $_SERVER['SERVER_PROTOCOL'] === 'RTA/x11') ||
+            ($CIDRAM['Config']['httpver']['rta_other'] && $_SERVER['SERVER_PROTOCOL'] !== 'RTA/x11')
+        ), 'Protocol denied (' . $Rebuilt . ')', $CIDRAM['Config']['httpver']['reason_message']);
     } else {
-        $Trigger($CIDRAM['Config']['httpver']['empty_unfamiliar'], 'Protocol denied', $CIDRAM['Config']['httpver']['reason_message']);
+        $Trigger($CIDRAM['Config']['httpver']['empty_unfamiliar'], 'Protocol denied (' . $Rebuilt . ')', $CIDRAM['Config']['httpver']['reason_message']);
     }
 };
 
