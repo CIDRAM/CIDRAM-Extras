4db4ffe74b394065c85461f22b21db59873580c6
Caleb Mazalevskis <maikuolan@gmail.com>
2021-12-01 00:32:26 +0800

    Aesthetic patch.

--- themes/fullmoon/pips.php
+++ themes/fullmoon/pips.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Full moon theme pips handler (last modified: 2020.08.04).
+ * This file: Full moon theme pips handler (last modified: 2021.11.30).
  */
 
 /** Prevents execution from outside of CIDRAM. */
@@ -16,6 +16,10 @@ if (!defined('CIDRAM')) {
     die('[CIDRAM] This should not be accessed directly.');
 }
 
-$CIDRAM['FE']['PIP_Left'] = 'R0lGODlhCAAIAKECAMrMyfv99P///////yH5BAEKAAEALAAAAAAIAAgAAAINjH+ga6vJIEDh0UmzKQA7';
-$CIDRAM['FE']['PIP_Right'] = 'R0lGODlhCAAIAKECAMrMyfv99P///////yH5BAEKAAEALAAAAAAIAAgAAAINjH+gmwvoUGBSSfOuKQA7';
 $CIDRAM['FE']['PIP_Key'] = 'R0lGODlhBwAJAKECAMrMyf3/+////////yH5BAEKAAEALAAAAAAHAAkAAAINjH+gyaaAAkQrznRbKAA7';
+$CIDRAM['FE']['PIP_Left'] = 'R0lGODlhCAAIAKECAMrMyfv99P///////yH5BAEKAAEALAAAAAAIAAgAAAINjH+ga6vJIEDh0UmzKQA7';
+$CIDRAM['FE']['PIP_Left_Invalid'] = 'R0lGODlhCAASAKECAJkCAMrMyQAAAAAAACH5BAEKAAAALAAAAAAIABIAAAIXhH+ha6vJYEDg0UmzzhZ1yGGeuIEXqRUAOw==';
+$CIDRAM['FE']['PIP_Left_Valid'] = 'R0lGODlhCAASAKECAAKZAMrMyQAAAAAAACH5BAEKAAIALAAAAAAIABIAAAIclH+ha6vJYEDi0Umz3hxk4FEhAh6ANxpnqnJHAQA7';
+$CIDRAM['FE']['PIP_Right'] = 'R0lGODlhCAAIAKECAMrMyfv99P///////yH5BAEKAAEALAAAAAAIAAgAAAINjH+gmwvoUGBSSfOuKQA7';
+$CIDRAM['FE']['PIP_Right_Invalid'] = 'R0lGODlhCAASAKECAJkCAMrMyQAAAAAAACH5BAEKAAAALAAAAAAIABIAAAIYhH+hmxvoEGBSSfOuvrZm+DVcuGHkdEoFADs=';
+$CIDRAM['FE']['PIP_Right_Valid'] = 'R0lGODlhCAASAKECAAKZAMrMyQAAAAAAACH5BAEKAAIALAAAAAAIABIAAAIblH+hmxvokGBSSfOu3hxc4EkhAh7laIBoyh0FADs=';
