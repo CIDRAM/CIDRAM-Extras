595ca2072a0f6c8184f6bb38310de15a1827a556
Caleb Mazalevskis <maikuolan@gmail.com>
2025-05-16 19:03:08 +0800

    Extras module update.

--- modules/module_extras.php
+++ modules/module_extras.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Optional security extras module (last modified: 2025.05.12).
+ * This file: Optional security extras module (last modified: 2025.05.16).
  *
  * False positive risk (an approximate, rough estimate only): « [ ]Low [x]Medium [ ]High »
  */
@@ -161,11 +161,11 @@ $CIDRAM['ModuleResCache'][$Module] = function () use (&$CIDRAM) {
 
         /** Probing for webshells/backdoors. */
         if ($Trigger(preg_match(
-            '~(?:^|[/?])(?:perl.alfa|search/label/php-shells)(?:$|[/?])~',
+            '~(?:^|[/?])(?:fierzashell\.html?|perl.alfa|search/label/php-shells)(?:$|[/?])~',
             $LCNrURI
         ), 'Probing for webshells/backdoors')) {
             $CIDRAM['Reporter']->report([15, 20, 21], ['Caught probing for webshells/backdoors. Host might be compromised.'], $CIDRAM['BlockInfo']['IPAddr']);
-        } // 2025.05.12
+        } // 2025.05.12 mod 2025.05.16
 
         /** Probing for exposed Git data. */
         if ($Trigger(preg_match('~\.git(?:config)?(?:$|\W)~', $LCNrURI), 'Probing for exposed git data')) {
